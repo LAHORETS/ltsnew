01f3ae2396be09d71676b0190b0c09fb7a0bd932
LAHORETS <77092775+LAHORETS@users.noreply.github.com>
2021-06-08 00:02:02 +0500

    Update account_move.py

--- discount_account_invoice/models/account_move.py
+++ discount_account_invoice/models/account_move.py
@@ -59,8 +59,9 @@ class AccountMove(models.Model):
         if self.invoice_line_ids:
             for line in self.invoice_line_ids:
                 if line.product_id.type == 'consu' or 'product':
-                    self.case3 = True
-                    self.case4 = False
+                    if self.partner_id.fbr_stn_active == True:
+                        self.case3 = True
+                        self.case4 = False
                 else:
                     self.case3 = False
         else:
@@ -72,8 +73,9 @@ class AccountMove(models.Model):
         if self.invoice_line_ids:
             for line in self.invoice_line_ids:
                 if line.product_id.type == 'service':
-                    self.case4 = True
-                    self.case3 = False
+                    if self.partner_id.fbr_stn_active == True:
+                        self.case4 = True
+                        self.case3 = False
                 else:
                     self.case4 = False
         else:
