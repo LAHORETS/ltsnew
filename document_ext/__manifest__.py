275c368416a7a4875149f901e638a9fbb27a98a6
LAHORETS <77092775+LAHORETS@users.noreply.github.com>
2021-06-17 11:09:21 +0500

    Add files via upload

---
+++ document_ext/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Document Extension',
+    'version': '13.0.1.0',
+    'category': 'Operations/Documents',
+    'summary': """Increase width of Search Panel""",
+    'description': """
+Increase width of Search Panel
+==================
+This app increases width of Search Panel.
+""",
+    'depends': ['documents'],
+    'data': [
+        'views/assets.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
