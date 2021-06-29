be4b90dfa03f06cedfb54d263b5f81901dcfdca9
LAHORETS <lts.viltco@gmail.com>
2021-06-29 14:30:23 +0500

    [Add] initial commit for Document module

---
+++ document_name_change/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Documents name change',
+    'version' : '1.0',
+    'license': 'OPL-1',
+    'depends' : ['documents'],
+    
+    'qweb': [
+        "static/src/xml/*.xml",
+    ],
+    
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+}
